8ea5f61d5ec33d94ba5a2375ccc8acda5a13399c
Luca <mylince@gmail.com>
2020-02-26 02:34:47 +0100

    Sbagliato a caricare il main.js di prima

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-/*
+/*/*
 
 1. Il computer deve generare 16 numeri casuali tra 1 e 100.
 2. In seguito deve chiedere all’utente di inserire un numero alla volta, sempre compreso tra 1 e 100.
@@ -13,9 +13,9 @@ Al termine della partita il software deve comunicare il punteggio, cioè il nume
 
 
 // Genero numeri random da 1 a 100:
-var somma = generaRandomCento(1, 100); //----> Variabile scartata; è giusto per far capire che questa mi creerà numeri da 1 a 100
+var somma = generaRandomCento(1, 100); //----> Variabile per far capire che questa mi creerà numeri da 1 a 100
 
- function generaRandomCento(min, max) {
+function generaRandomCento(min, max) {
      var centoRandom = Math.floor(Math.random() * (max - min + 1) ) + min;
      return centoRandom;
  }
@@ -33,41 +33,27 @@ while ( array.length < 16){
 
 console.log(array);
 
-
 //2. Il computer deve chiedere all’utente di inserire un numero alla volta, sempre compreso tra 1 e 100.
-
-
 //3 Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
 
-var inserisciNumero = parseInt(prompt('Inserisci un numero'));
-var lista = [];
-var listaBombe = 0;
-var controllo = controlloBombe(listaBombe, inserisciNumero)
-
- function controlloBombe(elem1, elem2){
-   var find = false;
-   for (var i = 0; i < lista.length; i++) {
-  if (lista[i] == numero) {
-    find = true;
-   }
-   }
-   return find;
+var number = parseInt(prompt("Inserisci un numero da 1 a 100; hai 3 tentativi per vincere"));
+switch (number <= 16) {
+    case somma == number:
+        alert("BOOOOM! Hai vinto!"); // ----> scatta l'alert vittoria se il numero è compreso
+        break;
+    case somma !== number:
+        alert ('sbagliato'); // ----> scatta l'alert sbagliato se non è compreso
+        parseInt(prompt('riprova'));
+        alert ('Hai finito i tentativi; hai perso');
+        default:
 }
 
+console.log(number);
 
-var numeriConsentiti = [];
-var conteggio = 0;
-while ( conteggio < 84 && (controlloBombe(listaBombe, inserisciNumero)) != true){
-    var inserisciNumero = parseInt(prompt('Inserisci un numero da 1 a 100'));
-    conteggio = conteggio + 1;
-    if (controlloBombe(listaBombe, inserisciNumero)) { //<-- se questa condizione risulta vera comunico
-      alert('hai perso hai provato ' + conteggio + ' volte prima di beccare la bomba');
-  }else if (numeriConsentiti.includes(random) == false){
-     numeriConsentiti.push(random); //<-- se il numero inserito non è presente nella lista allora viene aggiunto
-     conteggio = conteggio + 1; //<-- tengo traccia della quantita di tentativi riusciti
- }else if (numeriConsentiti.includes(random)){ //<-- se il numero è gia presente nella lista allora dico di inserirne un'altro
-     alert('numero gia inserito, inseriscine un/altro');
- }else if (numeriConsentiti.length == possibilita){
-     alert('hai vinto');
-    }
-  }
+
+
+var somma = somma1(random, number);
+
+function somma1(num1, num2){
+    return num1 == num2
+}
